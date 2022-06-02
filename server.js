c1875ce8e7de9c63f44c313f1bcf744c106ea1f2
aditya083-etce <you@example.com>
2022-06-03 02:41:01 +0530

    creation and styling

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
-const ejs = require('ejs')
 const path = require('path')
+const bodyParser = require("body-parser");
+var favicon = require('serve-favicon');
 const expressLayout = require('express-ejs-layouts')
+
+// Assets
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
 // app.use(expressLayout)
 app.set('views', path.join(__dirname, 'resources/views'))
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
     res.render('home');

29c3300bda639912c3d74a967bfa0b6fa8e7fbcb
aditya083-etce <you@example.com>
2022-06-03 16:34:55 +0530

    added cart auth

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express();
 const path = require('path')
 const bodyParser = require("body-parser");
 var favicon = require('serve-favicon');
@@ -9,15 +9,26 @@ const expressLayout = require('express-ejs-layouts')
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
-// app.use(expressLayout)
-app.set('views', path.join(__dirname, 'resources/views'))
+app.use(expressLayout)
+app.set('views', path.join(__dirname, 'resources/views'));
 app.use(bodyParser.urlencoded({extended: false}));
-app.set("view engine", "ejs")
-
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render('home');
-})
+});
+
+app.get("/cart", (req, res) => {
+    res.render('customers/cart');
+});
+
+app.get("/register", (req, res) => {
+    res.render('auth/register');
+});
+
+app.get("/login", (req, res) => {
+    res.render('auth/login');
+});
 
 const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
